e5ac3b0963529971dd755cc64e5623ca50da2488
coderylicode <rylidavis94@gmail.com>
2021-10-13 01:21:56 -0700

    Finished HeroSection

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from '../Navbar'
-import { HeroContainer} from './HeroElements'
+import { HeroContainer, HeroContent, HeroItems, HeroH2, HeroP, HeroBtn, } from './HeroElements'
 
 const Hero = () => {
     return (
     <HeroContainer>
         <Navbar />
+        <HeroContent>
+            <HeroItems>
+                <HeroH2>Erald and Louis 
+                    <br></br>
+                    Greatest Meatballs Ever</HeroH2>
+                <HeroP>Ready in 10 minutes</HeroP>
+                <HeroBtn>Place Order</HeroBtn>
+            </HeroItems>
+        </HeroContent>
     </HeroContainer>
     );
 };
